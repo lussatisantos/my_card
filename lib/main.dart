f79fc8853b76e7568020d203d175dd8a5fa38029
Lussati Santos <lussatisantos10@gmail.com>
2024-12-01 21:03:43 +0100

    height and width

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -12,6 +11,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: Container(
+            margin: EdgeInsets.fromLTRB(100.0, 200.0, 10.0, 200.0),
+            padding: EdgeInsets.only(left: 50.0),
+            height: 100.0,
+            width: 100.0,
             color: Colors.white,
             child: Text('Hello World!'),
           ),
