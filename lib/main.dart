afd2f7eb08532140b34d1236873d4f7aff10712e
Lussati Santos <lussatisantos10@gmail.com>
2024-12-02 19:10:33 +0100

    column and row
    adjust column and row at flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,17 +9,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.fromLTRB(100.0, 200.0, 10.0, 200.0),
-            padding: EdgeInsets.only(left: 50.0),
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            child: Text('Hello World!'),
-          ),
-        ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              //margin: EdgeInsets.fromLTRB(100.0, 200.0, 10.0, 200.0),
+              // padding: EdgeInsets.only(left: 50.0),
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.yellow,
+              child: Text('Container 3'),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.green,
+              child: Text('Container 4'),
+            ),
+            SizedBox(height: 20.0,),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+          ],
+        )),
       ),
     );
   }
