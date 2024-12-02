f8eb202715654584331601bd70a529b827ec5b83
Lussati Santos <lussatisantos10@gmail.com>
2024-12-02 21:07:34 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              //margin: EdgeInsets.fromLTRB(100.0, 200.0, 10.0, 200.0),
-              // padding: EdgeInsets.only(left: 50.0),
-              height: 100.0,
-              width: 100.0,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            SizedBox(height: 20.0,),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.yellow,
-              child: Text('Container 3'),
-            ),
-            SizedBox(height: 20.0,),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.green,
-              child: Text('Container 4'),
-            ),
-            SizedBox(height: 20.0,),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-          ],
-        )),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('imgs/profile.png'),
+                radius: 50.0,
+              ),
+              Text(
+                'Lussati Santos',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                '   ADMINISTRATIVO | SOFTWARE DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.deepOrange.shade100,
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
